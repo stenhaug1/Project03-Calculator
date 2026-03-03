4f8c3691eac79e21ce7ba1597f9866066025434c
stenhaug1 <thomas.p26@proton.me>
2026-03-03 11:22:36 +0100

    first setup of the code

--- Program.cs
+++ Program.cs
@@ -1,9 +1,32 @@
-﻿namespace week_3;
+﻿Console.WriteLine("Input first if you want (+,-,*,/) and then2 numbers and we will give you the result");
 
-class Program
+string i1 = Console.ReadLine();
+Console.WriteLine(" you Have chosen " + i1);
+Console.WriteLine("Input your first number please :)");
+double tall1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Input your seccond number please :)");
+double tall2 = double.Parse(Console.ReadLine());
+
+
+
+if (i1 == "+")
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
-    }
+    Console.WriteLine("The sum of the numbers are " + (tall1 + tall2));
+}
+
+else if (i1 == "-")
+    Console.WriteLine("The substracted value is " + (tall1 - tall2));
+
+else if (i1 == "*")
+{
+    Console.WriteLine("The multiplied value is " + (tall1 * tall2));
+}
+
+else if (i1 == "/")
+{
+    Console.WriteLine("The diveded value is " + (tall1 / tall2));
+}
+else
+{
+    Console.WriteLine("Please choose your choice of sum, substraction, multiply or divination");
 }
