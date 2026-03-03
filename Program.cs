1a2f863113a8b23d57dd399cd2e3bbf7e0fded01
stenhaug1 <thomas.p26@proton.me>
2026-03-03 12:38:49 +0100

    added a while loop for continued calculations

--- Program.cs
+++ Program.cs
@@ -1,32 +1,50 @@
-﻿Console.WriteLine("Input first if you want (+,-,*,/) and then2 numbers and we will give you the result");
+﻿int i2 = 0;
 
-string i1 = Console.ReadLine();
-Console.WriteLine(" you Have chosen " + i1);
-Console.WriteLine("Input your first number please :)");
-double tall1 = double.Parse(Console.ReadLine());
-Console.WriteLine("Input your seccond number please :)");
-double tall2 = double.Parse(Console.ReadLine());
-
-
-
-if (i1 == "+")
+while (i2 == 0)
 {
-    Console.WriteLine("The sum of the numbers are " + (tall1 + tall2));
-}
+    Console.WriteLine("Input first if you want (+,-,*,/) and then2 numbers and we will give you the result");
 
-else if (i1 == "-")
-    Console.WriteLine("The substracted value is " + (tall1 - tall2));
+    string i1 = Console.ReadLine();
+    Console.WriteLine(" you Have chosen " + i1);
+    Console.WriteLine("Input your first number please :)");
+    double tall1 = double.Parse(Console.ReadLine());
+    Console.WriteLine("Input your seccond number please :)");
+    double tall2 = double.Parse(Console.ReadLine());
 
-else if (i1 == "*")
-{
-    Console.WriteLine("The multiplied value is " + (tall1 * tall2));
-}
+    if (i1 == "+")
+    {
+        Console.WriteLine("The sum of the numbers are " + (tall1 + tall2));
+        i2 = 1;
+    }
 
-else if (i1 == "/")
-{
-    Console.WriteLine("The diveded value is " + (tall1 / tall2));
-}
-else
-{
-    Console.WriteLine("Please choose your choice of sum, substraction, multiply or divination");
+    else if (i1 == "-")
+    {
+        Console.WriteLine("The substracted value is " + (tall1 - tall2));
+        i2 = 1;
+    }
+    else if (i1 == "*")
+    {
+        Console.WriteLine("The multiplied value is " + (tall1 * tall2));
+        i2 = 1;
+    }
+
+    else if (i1 == "/")
+    {
+        Console.WriteLine("The diveded value is " + (tall1 / tall2));
+        i2 = 1;
+    }
+    else
+    {
+        Console.WriteLine("Please choose your choice of sum, substraction, multiply or divination");
+    }
+
+    Console.WriteLine("Would you like to calculate more?");
+    String i3 = Console.ReadLine();
+    if (i3 == "Y" || i3 == "y" || i3 == "yes")
+        i2 = 0;
+    else
+    {
+        i2 = 1;
+    }
+    ;
 }
